095159327a48426b3f5935ac923da426509f06ce
Dmitriy Polozov <dipol.xxl@gmail.com>
2012-11-11 15:37:46 +0300

    Refactoring: add before_filter for checking_the_date.

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,17 +1,25 @@
 class CurrenciesController < ApplicationController
-
+  before_filter :checking_the_date
   respond_to :json
+
+  def checking_the_date
+    begin
+      @date = params[:month].presence.try(:to_date) || Date.today
+    rescue
+      # ToDo: подумать над отловом типа ошибок!
+      render :text => "Invalid date format! Expected 'YYYY-MM-DD'."
+    end
+  end
 
   # GET /currencies.json?month=  or  GET /currencies.json?month=2012-12-21
   def index
-    date = params[:month] == "" ? Time.now.to_date : params[:month].to_date
-    @rates = Rate.at_month(date)
+    @rates = Rate.at_month(@date)
     respond_with @rates
   end
 
   # GET /currencies/USD.json?month=2012-10-01
   def show
-    @currency = Currency.at_code(params[:id]).first.rates.at_month(params[:month].to_date)
+    @currency = Currency.at_code(params[:id]).first.rates.at_month(@date)
     respond_with @currency
   end
 end
